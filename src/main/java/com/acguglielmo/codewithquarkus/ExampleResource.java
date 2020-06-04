01150667a0978f3af14d574f5fa615abeee79589
Antonio Guglielmo <antonio_guglielmo@hotmail.com>
2020-06-04 17:56:24 -0300

    Refactor ExampleResource class to use lombok annotation

--- src/main/java/com/acguglielmo/codewithquarkus/ExampleResource.java
+++ src/main/java/com/acguglielmo/codewithquarkus/ExampleResource.java
@@ -1,25 +1,17 @@
 package com.acguglielmo.codewithquarkus;
 
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import lombok.RequiredArgsConstructor;
+
 @Path("/hello")
+@RequiredArgsConstructor
 public class ExampleResource {
 
-    private ExampleService exampleService;
-
-    public ExampleResource() {
-    }
-    
-    @Inject
-    public ExampleResource(final ExampleService exampleService) {
-
-        this.exampleService = exampleService;
-
-    }
+    private final ExampleService exampleService;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
